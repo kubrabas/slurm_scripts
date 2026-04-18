0b5e62ec6e35f4eca77cd1efa960e6d16f0be9ac
kubrabas <kubraaabasss@gmail.com>
2026-04-18 22:53:28 +0200

    edit

--- Spring2026MC/summary/submit_i3_summary.sh
+++ Spring2026MC/summary/submit_i3_summary.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
-#SBATCH --job-name=I3Photons_Summary
+#SBATCH --job-name=I3PhotonsSummarySpring2026MC
 #SBATCH --time=48:00:00
-#SBATCH --account=def-nahee
+#SBATCH --account=rpp-nahee
 #SBATCH --mem=96G
 #SBATCH --cpus-per-task=8
-#SBATCH --output=/home/kbas/scratch/2026_Spring_MC/logs/summary_i3_%j.out
-#SBATCH --error=/home/kbas/scratch/2026_Spring_MC/logs/summary_i3_%j.out
+#SBATCH --output=/home/kbas/scratch/Spring2026MC/Logs/SummaryI3_i3_%j.out
+#SBATCH --error=/home/kbas/scratch/Spring2026MC/Logs/SummaryI3_i3_%j.out
 
 set -euo pipefail
 
-OUTDIR="/project/def-nahee/kbas/Graphnet-Applications/Playground/2026_Spring_MC/Datasets"
-LOGDIR="/home/kbas/scratch/2026_Spring_MC/logs"
+MC_NAME="SPRING2026MC"
+
+OUTDIR="/project/def-nahee/kbas/Graphnet-Applications/Metadata/DatasetStatistics/Spring2026MC"
+LOGDIR="/home/kbas/scratch/Spring2026MC/Logs"
 mkdir -p "${LOGDIR}"
 mkdir -p "${OUTDIR}"
 
 echo "--- HOST: JOB=${SLURM_JOB_ID:-} HOST=$(hostname)"
+echo "--- MC_NAME: ${MC_NAME}"
 echo "--- OUTDIR: ${OUTDIR}"
 echo "--- CPUS_PER_TASK: ${SLURM_CPUS_PER_TASK:-unset}"
 
-module --force-purge
+module --force purge
 module load StdEnv/2020 gcc/11.3.0 apptainer scipy-stack/2023b
 
 CONTAINER="/cvmfs/software.pacific-neutrino.org/containers/itray_v1.17.1"
 PONE_OFFLINE="/cvmfs/software.pacific-neutrino.org/pone_offline/v1.2"
 PONESRCDIR="/project/6008051/pone_simulation/pone_offline"
 
-PYTHON_SCRIPT="/project/def-nahee/kbas/Graphnet-Applications/Playground/2026_Spring_MC/Datasets/prepare_summary_for_I3.py"
+PYTHON_SCRIPT="/project/def-nahee/kbas/Graphnet-Applications/DataPreperation/DatasetStatistics/prepare_summary_for_I3.py"
 
 unset I3_SHELL || true
 unset I3_BUILD || true
 
 apptainer exec \
-  -B /usr/local/scratch/ \
+  -B /localscratch/ \
   -B /cvmfs/software.pacific-neutrino.org/ \
   -B /etc/OpenCL \
-  --nv \
   "${CONTAINER}" \
   bash -lc " \
     set -euo pipefail; \
@@ -42,12 +44,12 @@ apptainer exec \
     export PONESRCDIR='${PONESRCDIR}'; \
     export PYTHONUNBUFFERED=1; \
     python3 -u '${PYTHON_SCRIPT}' \
-      --workers ${SLURM_CPUS_PER_TASK:-1} \
+      --mc-name '${MC_NAME}' \
+      --workers '${SLURM_CPUS_PER_TASK:-1}' \
+      --out-csv '${OUTDIR}/i3summary.csv' \
+      --out-txt '${OUTDIR}/i3summary.txt' \
   "
 
 rc=$?
-echo "--- HOST: Job finished (container exited with code ${rc})."
+echo "--- Job finished (rc=${rc})"
 exit $rc
-
-
-not finished and checked
